c02172ae6b0641340862e11820346a32fb3147b9
gfatama <gfatama@are.com>
2024-10-26 15:17:52 -0500

    feat: button show list

--- app/components/ui/showItems.tsx
+++ app/components/ui/showItems.tsx
@@ -2,7 +2,13 @@ import { useContext, useEffect, useState } from "react"
 import { ListItemContext, Item } from "../context/listContext";
 
 export default function ShowItem() {
+    enum ButtonName {
+        Show = 'Show', 
+        Hide = 'Hide'
+    }
     const [ itemsContext ] = useContext(ListItemContext);
+    // const [ buttonName, setButtonName ] = useState<ButtonName>(ButtonName.Hide)
+    const [ showList, setShowList ] = useState(true);
 
     useEffect(() => {
         /* 
@@ -17,16 +23,38 @@ export default function ShowItem() {
         } */
     }, [itemsContext]);
 
+    const clickButtonList = () => {
+        setShowList(!showList);
+        // setButtonName( (buttonName: ButtonName) => buttonName === ButtonName.Hide ? ButtonName.Show : ButtonName.Hide);
+    }
+
     const listComponents = itemsContext.map((item: Item, index: number) => 
         <li key={ `${index}-${item.title}` }>
             { item.title }
         </li>
-    )
+    );
+
     return (
-        <div className="m-5 grid grid-rows-3 grid-flow-col">
+        <div className="m-6 grid grid-rows-3 grid-flow-col">
             <div className="flex flex-col">
-                <h3 className="mb-2 font-semibold text-lg ">Questions List</h3>
-                <ul className="font-light">{listComponents}</ul>
+                <div className="flex justify-between content-center mb-4">
+                    <p className="font-semibold text-lg ">Questions List</p>
+                    <button 
+                    disabled={ itemsContext.length === 0 }
+                    onClick={clickButtonList} 
+                    className="bg-sky-500 text-white hover:bg-sky-700 px-5 py-1 rounded text-xs font-semibold disabled:opacity-5xs0"
+                    >
+              {/*           { buttonName } */}
+                        { showList ? ButtonName.Show : ButtonName.Hide }
+                    </button>
+                </div>
+                <>
+                {
+                    showList ? 
+                    <ul className="font-light">{listComponents}</ul> : <></>
+                }
+                </>
+                
             </div>
         </div>
 
